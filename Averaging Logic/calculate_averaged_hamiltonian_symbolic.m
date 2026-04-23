a011ab03b5b49e1559e508c61d7dcfe01ef6f044
moses <moses@mosesPC>
2026-04-22 21:52:35 -0400

    Updated symbolic differentiation of averaged hamiltonian

--- Averaging Logic/calculate_averaged_hamiltonian_symbolic.m
+++ Averaging Logic/calculate_averaged_hamiltonian_symbolic.m
@@ -1,64 +1,66 @@
 function H_avg = calculate_averaged_hamiltonian_symbolic(x, lambda, params, L_nodes, w)
-    
+
     alph = x(8);
     m = x(9);
-    
+
     H_avg = sym(0);
-    
+
     for i = 1:length(L_nodes)
         L_i = L_nodes(i);
-    
-        % Replace the longitude state with the quadrature longitude
+
         x_i = x;
         x_i(6) = L_i;
-    
+
         A = get_MEE_A_matrix(x_i, params.mu);
         B = get_MEE_B_matrix(x_i, params.mu);
-        gamma_RTN = J2pert(x_i, params);
-    
+
+        if params.use_J2
+            gamma_RTN = J2pert(x_i, params.mu, params.RE, params.J2);
+        else
+            gamma_RTN = sym([0; 0; 0]);
+        end
+
         lam_MEE = lambda(1:6);
         lam_m   = lambda(9);
-    
+        lam_t   = lambda(7);
+
         B_trans_lam = B.' * lam_MEE;
-    
-        % Smooth norm regularization instead of if/else
         norm_B_trans_lam = sqrt(B_trans_lam.' * B_trans_lam + params.eps_u^2);
-    
         u_hat = -B_trans_lam / norm_B_trans_lam;
-    
-        S = -(params.c / m) * norm_B_trans_lam - lam_m + 1;
-    
+
         eps_S = params.epsilon_S;
+        S = -(params.c / m) * norm_B_trans_lam - lam_m + 1;
         sigma = 0.5 * (1 - S / sqrt(S^2 + eps_S^2));
-    
-        k_e = sym(1);
-        T = params.T_min + (params.T_max - params.T_min) * k_e * sigma;
-    
+
+        T = params.T_min + (params.T_max - params.T_min) * sigma;
+
+        time_costate_term = lam_t * alph;
+
         cost_term = alph * (params.T_min / params.c) + ...
-            alph * ((params.T_max - params.T_min) / params.c) * ...
-            k_e * (sigma - eps_S * sqrt(sigma - sigma^2));
-    
+                    alph * ((params.T_max - params.T_min) / params.c) * ...
+                    (sigma - eps_S * sqrt(sigma - sigma^2));
+
         dyn_term = alph * (lam_MEE.' * A) + ...
-            alph * (lam_MEE.' * (B * (u_hat * (T / m) + gamma_RTN)));
-    
+                   alph * (lam_MEE.' * (B * (u_hat * (T / m) + gamma_RTN)));
+
         mass_costate_term = -alph * lam_m * (T / params.c);
-    
-        H_i = cost_term + dyn_term + mass_costate_term;
-    
+
+        H_i = cost_term + dyn_term + mass_costate_term + time_costate_term;
+
         p = x_i(1);
         f = x_i(2);
         g = x_i(3);
-    
-        r = p / (1 + f*cos(L_i) + g*sin(L_i));
+
+        q = 1 + f*cos(L_i) + g*sin(L_i);
+        r = p / q;
+
         dt_dL = r^2 / sqrt(params.mu * p);
-    
+
         a = p / (1 - f^2 - g^2);
         P = 2*pi*sqrt(a^3 / params.mu);
-    
+
         s_i = (2*pi / P) * dt_dL;
-    
+
         H_avg = H_avg + sym(0.5) * w(i) * s_i * H_i;
     end
-    
-    H_avg = H_avg;
 end
--- Averaging Logic/differentiate_averaged_hamiltonian.m
+++ Averaging Logic/differentiate_averaged_hamiltonian.m
@@ -1,15 +1,19 @@
-function derivs = differentiate_averaged_hamiltonian(q)
-    
+function derivs = differentiate_averaged_hamiltonian(q, use_J2)
+
+    if nargin < 2
+        use_J2 = true;
+    end
+
     syms p f g h k L t alph m real
-    syms lam_p lam_f lam_g lam_h lam_k lam_L lam_t lam_alpha lam_m real
+    syms lam_p lam_f lam_g lam_h lam_k lam_L lam_t lam_alph lam_m real
     syms mu_g c T_min T_max epsilon_S eps_u RE J2 real
-    
+
     x = [p; f; g; h; k; L; t; alph; m];
-    lambda = [lam_p; lam_f; lam_g; lam_h; lam_k; lam_L; lam_t; lam_alpha; lam_m];
-    
+    lambda = [lam_p; lam_f; lam_g; lam_h; lam_k; lam_L; lam_t; lam_alph; lam_m];
+
     [z, w] = lgwt(q, -1, 1);
     L_nodes = pi * z;
-    
+
     params.mu = mu_g;
     params.c = c;
     params.T_min = T_min;
@@ -18,12 +22,13 @@ function derivs = differentiate_averaged_hamiltonian(q)
     params.eps_u = eps_u;
     params.RE = RE;
     params.J2 = J2;
-    
+    params.use_J2 = use_J2;
+
     H_avg = calculate_averaged_hamiltonian_symbolic(x, lambda, params, L_nodes, w);
-    
+
     dH_dx = jacobian(H_avg, x).';
     dH_dlambda = jacobian(H_avg, lambda).';
-    
+
     derivs.H_avg = H_avg;
     derivs.dH_dx = dH_dx;
     derivs.dH_dlambda = dH_dlambda;
